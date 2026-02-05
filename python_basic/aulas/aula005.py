eed2dc934f146c20945174b94d06f09f7e8b0418
Vitor Zavan <vitorzavanj@gmail.com>
2026-02-05 17:38:01 -0300

    aulas

--- python_basic/aulas/aula005.py
+++ python_basic/aulas/aula005.py
@@ -30,3 +30,34 @@ formato = string.format(
 
 
 print(formato)
+
+print('\n__ Mais sobre f-strings__\n')
+
+'''
+Formatação básica de strings
+s - string
+d - int
+f - float
+.<número de dígitos>f
+x ou X - Hexadecimal
+(Caractere)(><^)(quantidade)
+> - Esquerda
+< - Direita
+^ - Centro
+= - Força o número a aparecer antes dos zeros
+Sinal - + ou -
+Ex.: 0>-100,.1f
+Conversion flags - !r !s !a 
+'''
+
+variavel_um = 'ABC'
+variavel_dois = 1000.986485
+
+print(f'{variavel_um}')
+print(f'{variavel_um:>10}')
+print(f'{variavel_um:<10}')
+print(f'{variavel_um:#^10}')
+print(f'{variavel_dois:+,.2f}')
+print(f'{variavel_um!r}')
+print(f'{variavel_um!s}')
+print(f'{variavel_um!a}')
--- python_basic/aulas/aula009.py
+++ python_basic/aulas/aula009.py
@@ -1,3 +1,18 @@
 '''
 Docstring for python_basic.aulas.aula009
+
+Interpolação de strings
 '''
+
+# s - string
+# d e i - int
+# f - float
+# x e X - Hexadecimal (ABCDEF0123456789)
+
+nome = 'Vitor'
+valor = 10.95
+variavel = '%s, tem um energético que custou R$ %f' % (nome, valor)
+exadecial = 1780
+
+print(variavel)
+print ('O exadecial de %d, é %08X' % (exadecial, exadecial))
