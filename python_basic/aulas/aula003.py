3d807c7acefa6b335abe0882bcf01b4fad7866c9
Vitor Zavan <vitorzavanj@gmail.com>
2026-02-08 01:06:37 -0300

    aulas

--- python_basic/aulas/aula003.py
+++ python_basic/aulas/aula003.py
@@ -6,7 +6,7 @@ Introdução a váriaveis
 
 nome_completo = 'Vitor Zavan'
 idade = 29
-maior_de_idade = lambda idade: idade >=18 #função anônima, mais sobre, no futuro
+maior_de_idade = lambda idade: idade >=18 # Função anônima, mais sobre, no futuro
 curso_ativo = True
 nota = 9.8
 
@@ -19,3 +19,26 @@ formulario_aluno = {
 }
 for x, y in formulario_aluno.items():
     print(x, y)
+
+
+'''
+Variáveis e constantes
+'''
+# Variável
+velocidade = 60 # Velocidade atual do carro
+local_carro = 100 # Local em que o carro está na estrada
+
+
+# Constante
+RADAR_1 = 60 # Velocidade máxima do radar 1
+LOCAL_1 = 100 # Local onde o radar 1 está
+RADAR_RANGE = 1 # A distância onde o radar pega
+
+if (
+    velocidade > RADAR_1
+    and LOCAL_1 - RADAR_RANGE <= local_carro <= LOCAL_1 + RADAR_RANGE
+):
+    print('Multa')
+
+else:
+    print('Boa viagem')
--- python_basic/aulas/aula011.py
+++ python_basic/aulas/aula011.py
@@ -2,4 +2,25 @@
 Docstring for python_basic.aulas.aula011
 
 Introdução ao Try Except
+
+Try -> Tente executar o código
+
+Except -> Ocorreu um erro ao executar
 '''
+
+numero_str = input(
+    'Vou dobrar o número que vc digitar: '
+)
+
+try:
+    numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
+    print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+except:
+    print('Isso não é um número')
+
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float * 2:.2f}')
+# else:
+#     print('Isso não é um número')
